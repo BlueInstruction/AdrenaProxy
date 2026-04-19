1941d91a4c4ff890bb9e1bb2c03afbfe020141fc
BlueInstruction <252104246+BlueInstruction@users.noreply.github.com>
2026-04-19 01:30:41 +0100

    Update sgsr1_pass.h

--- adrena_core/include/adrena_core/sgsr1_pass.h
+++ adrena_core/include/adrena_core/sgsr1_pass.h
@@ -5,22 +5,34 @@
 
 namespace adrena {
 
+enum class SGSROperationMode : uint32_t {
+    RGBA = 1,
+    RGBY = 3,
+    LERP = 4
+};
+
 struct SGSRParams {
     ID3D12Resource* color    = nullptr;
     ID3D12Resource* depth    = nullptr;
     ID3D12Resource* motion   = nullptr;
     ID3D12Resource* output   = nullptr;
-    float    sharpness       = 0.80f;
+
+    float    sharpness       = 2.0f;
     uint32_t renderWidth     = 0;
     uint32_t renderHeight    = 0;
     uint32_t displayWidth    = 0;
     uint32_t displayHeight   = 0;
+
+    SGSROperationMode operationMode    = SGSROperationMode::RGBA;
+    float             edgeThreshold    = 8.0f / 255.0f;
+    bool              useEdgeDirection = false;
 };
 
 class SGSR1Pass {
 public:
     SGSR1Pass() = default;
     ~SGSR1Pass();
+
     SGSR1Pass(const SGSR1Pass&) = delete;
     SGSR1Pass& operator=(const SGSR1Pass&) = delete;
 
@@ -39,16 +51,20 @@ private:
     void Transition(ID3D12GraphicsCommandList* cl, ID3D12Resource* res,
                     D3D12_RESOURCE_STATES before, D3D12_RESOURCE_STATES after);
 
-    ID3D12Device*           m_device     = nullptr;
-    ID3D12RootSignature*    m_rootSig    = nullptr;
-    ID3D12PipelineState*    m_pso        = nullptr;
-    ID3D12DescriptorHeap*   m_srvHeap    = nullptr;
-    ID3D12DescriptorHeap*   m_samplerHeap= nullptr;
-    ID3D12Resource*         m_intermediate= nullptr;
-    uint32_t m_renderW = 0, m_renderH = 0;
-    uint32_t m_displayW = 0, m_displayH = 0;
-    DXGI_FORMAT m_format = DXGI_FORMAT_UNKNOWN;
-    bool m_initialized = false;
+    ID3D12Device*           m_device        = nullptr;
+    ID3D12RootSignature*    m_rootSig       = nullptr;
+    ID3D12PipelineState*    m_pso           = nullptr;
+    ID3D12DescriptorHeap*   m_srvHeap       = nullptr;
+    ID3D12DescriptorHeap*   m_samplerHeap   = nullptr;
+    ID3D12Resource*         m_intermediate  = nullptr;
+
+    DXGI_FORMAT m_format     = DXGI_FORMAT_UNKNOWN;
+    uint32_t    m_renderW    = 0;
+    uint32_t    m_renderH    = 0;
+    uint32_t    m_displayW   = 0;
+    uint32_t    m_displayH   = 0;
+    UINT        m_srvIncSize = 0;
+    bool        m_initialized = false;
 };
 
 } // namespace adrena
